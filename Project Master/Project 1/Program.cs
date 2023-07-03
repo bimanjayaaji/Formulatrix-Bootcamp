348ce0c5d488197d549544b878d7020bf1c3b7e6
bimanjayaaji <anjay4705@gmail.com>
2023-07-03 12:11:55 +0700

    initialize commit

---
+++ Project Master/Project 1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("Hello, World!");
+
+class Program 
+{
+	static int myint = 2;
+	static string mystring = "i am a mystring var";
+	
+	static void Main()
+	{
+		Console.WriteLine("Hello, World!");
+		Console.WriteLine(myint);
+		Message();
+	}
+	
+	static void Message()
+	{
+		Console.WriteLine("Hello, Bang!");		
+		Console.WriteLine(mystring);
+	}
+}
