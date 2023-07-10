575897f9ce452c7169550c83e21fb28e2ead9127
bimanjayaaji <anjay4705@gmail.com>
2023-07-10 09:04:57 +0700

    adding gitignore

---
+++ Project Master Week 2/_Scratching/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Scratching;
+
+// WORKSPACE FOR 
+
+class Program
+{
+	static void Main()
+	{
+		
+	}
+}
---
+++ Project Master Week 2/ref-out-in/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Week2Day1;
+
+class Program
+{
+	static void Main()
+	{
+		int number = 10;
+		int result;
+
+		// Using 'ref'
+		MultiplyByTwoRef(ref number);
+		Console.WriteLine("Using 'ref': " + number);
+
+		// Using 'out'
+		MultiplyByTwoOut(out result);
+		Console.WriteLine("Using 'out': " + result);
+
+		// Using 'in'
+		int readonlyNumber = 5;
+		MultiplyByTwoIn(in readonlyNumber);
+	}
+	
+	static void MultiplyByTwoRef(ref int num)
+    {
+        num *= 2;
+    }
+
+    static void MultiplyByTwoOut(out int num)
+    {
+        num = 7;
+        num *= 2;
+    }
+
+    static void MultiplyByTwoIn(in int num)
+    {
+        // num *= 2;  // Compilation error: Cannot modify the read-only parameter
+        int result = num * 2;
+        Console.WriteLine("Using 'in': " + result);
+    }
+}
