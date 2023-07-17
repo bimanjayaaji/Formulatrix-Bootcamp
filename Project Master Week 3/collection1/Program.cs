35e900a778ce3d5709931f471a1db929430efa62
bimanjayaaji <anjay4705@gmail.com>
2023-07-17 12:08:13 +0700

    collection 1

---
+++ Project Master Week 3/collection1/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Diagnostics;
+namespace week3day1;
+
+class Program
+{
+	static void Main()
+	{
+		// ----- ARRAY -----
+		System.Console.WriteLine("---");
+		
+		// int[] myArray = new int[8];
+		// myArray[0] = 0;
+		// myArray[1] = 1;
+		// myArray[2] = 2;
+		// myArray[4] = 4;
+		// myArray[5] = 5;
+		// foreach (var x in myArray)
+		// {
+		// 	System.Console.WriteLine(x); // defaultnya yg belom keisi 0
+		// }
+		
+		// System.Console.WriteLine(myArray.ToString()); // ngeprint system.int32
+		
+		// ----- ARRAYLIST -----
+		System.Console.WriteLine("---");
+		
+		// ArrayList sayurs = new ArrayList();
+		
+		// sayurs.Add("sayur pisang");
+		// sayurs.Add('s');
+		// sayurs.Add(123);
+		// sayurs.Add(3.303030);
+		// //sayurs.Add(List<int>); // error
+		// List<int> buah = new List<int>();
+		// sayurs.Add(buah);
+		// //sayurs.Add(Dictionary<int,string>); // error
+		// Dictionary<int,string> myDict = new Dictionary<int,string>();
+		// sayurs.Add(myDict);
+		
+		// foreach (var x in sayurs)
+		// {
+		// 	System.Console.WriteLine(x);
+		// }
+		
+		// sayurs[3] = "tergantikan";
+		// System.Console.WriteLine(sayurs[3]);
+		
+		// ----- LIST -----
+		System.Console.WriteLine("---");
+		
+		// List<object> obj = new List<object>();
+		// obj.Add("ini objek");
+		// obj.Add("ini juga objek");
+		// obj.Add("masih objek");
+		// obj.Add(76);
+		
+		// foreach (var x in obj)
+		// {
+		// 	System.Console.Write(x + " | ");
+		// 	System.Console.WriteLine(x.GetType());
+		// }
+		
+		// ----- LINKED LIST -----
+		System.Console.WriteLine("---");
+		
+		// int iter = 1000000;
+		// LinkedList<int> linkedList = new LinkedList<int>();
+		// List<int> list = new List<int>();
+
+		// for (int i = 0; i < iter; i++)
+		// {
+		// 	linkedList.AddLast(i);
+		// }
+
+		// for (int i = 0; i < iter; i++)
+		// {
+		// 	list.Add(i);
+		// }
+
+		// Stopwatch linkedListSW = Stopwatch.StartNew();
+		// for (int i = 0; i < iter; i++)
+		// {
+		// 	int element = linkedList.First.Value;
+		// 	linkedList.RemoveFirst();
+		// }
+		// linkedListSW.Stop();
+
+		// Stopwatch listSW = Stopwatch.StartNew();
+		// for (int i = 0; i < iter; i++)
+		// {
+		// 	int element = list[0];
+		// 	list.RemoveAt(0);
+		// }
+		// listSW.Stop();
+
+		// Console.WriteLine("linkedListSW: " + linkedListSW.Elapsed); // linkedListSW: 00:00:00.0194503
+		// Console.WriteLine("listSW: " + listSW.Elapsed); // listSW: 00:02:50.9455432
+		
+		// ----- HASHSET -----
+		System.Console.WriteLine("---");
+		
+		// List<int> listiyanto = new List<int>();
+		// listiyanto.Add(1);
+		// listiyanto.Add(1);
+		// listiyanto.Add(1);
+		// listiyanto.Add(1);
+		// HashSet<int> hashbullah  = new HashSet<int>(listiyanto);
+		
+		// foreach (var x in hashbullah)
+		// {
+		// 	System.Console.WriteLine(x);
+		// }
+	}
+}
---
+++ Project Master Week 3/collection2/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace week3day1;
+
+class Program
+{
+	static void Main()
+	{
+		// initializing a deck of card
+		SetCards deck = new SetCards();
+		
+		// shuffling cards
+		deck.ShuffleCards();
+		
+		// take 5 cards out to a player
+		List<SingleCard> player1 = deck.Draw5Out();
+		
+		int counter = 1;
+		foreach (var card in player1)
+		{
+			Console.WriteLine($"{counter}. {card.jenis} | {card.rank}");
+			counter++;
+		}
+		
+		// printing card sequence
+		// deck.CardsSequence();
+	}
+}
+
+public enum Jenis
+{
+	Wajik = 0,
+	Hati, Keriting, Waru
+}
+
+public enum Rank
+{
+	Ace = 0,
+	Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+	Jack, Queen, King
+}
+
+class SingleCard
+{
+	public Jenis jenis {get;}
+	public Rank rank {get;}
+	
+	public SingleCard(Jenis x, Rank y)
+	{
+		jenis = x;
+		rank = y;
+	}
+}
+
+class SetCards
+{
+	private List<SingleCard> setCardComplete = new List<SingleCard>();
+	private Jenis jenis = new Jenis();
+	private Rank rank = new Rank();
+	
+	public SetCards()
+	{
+		foreach (Jenis jenis in Enum.GetValues(typeof(Jenis)))
+		{
+			foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+			{
+				SingleCard card = new SingleCard(jenis,rank);
+				setCardComplete.Add(card);	
+			}
+		}
+	}
+	
+	public void ShuffleCards()
+	{
+		Random random = new Random();
+		int n = setCardComplete.Count;
+		
+		while (n > 1)
+		{
+			n--;
+			int k = random.Next(n + 1);
+			SingleCard value = setCardComplete[k];
+			setCardComplete[k] = setCardComplete[n];
+			setCardComplete[n] = value;
+		}
+	}
+	
+	public List<SingleCard> Draw5Out()
+	{
+		List<SingleCard> drawed5 = new List<SingleCard>();
+		
+		for (int drawed = 0  ; drawed <= 4 ; drawed++)
+		{
+			drawed5.Add(setCardComplete[setCardComplete.Count-1]);
+			setCardComplete.RemoveAt(setCardComplete.Count-1);
+		}
+		
+		return drawed5;
+	}
+	
+	public void CardsSequence()
+	{
+		int counter = 1;
+		foreach (var x in setCardComplete)
+		{
+			Console.WriteLine($"{counter}. {x.jenis} | {x.rank}");
+			counter++;
+		}
+	}
+}
