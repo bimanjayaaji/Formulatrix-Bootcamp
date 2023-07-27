f91a0662108db11e7b495cf2f73e3fe9eec0399a
bimanjayaaji <anjay4705@gmail.com>
2023-07-27 10:14:31 +0700

    resync

--- Project Master Week 3/collection4/Program.cs
+++ Project Master Week 3/collection4/Program.cs
@@ -4,19 +4,19 @@ class Program
 {
 	static void Main()
 	{
-		// ----- HASHTABLE -----
-		var hashtable = new Hashtable();
+		// // ----- HASHTABLE -----
+		// var hashtable = new Hashtable();
 		
-		hashtable.Add(1, "stunggal");
-		hashtable.Add("kalih", 2);
-		hashtable.Add(3, true);
+		// hashtable.Add(1, "stunggal");
+		// hashtable.Add("kalih", 2);
+		// hashtable.Add(3, true);
 		
-		System.Console.WriteLine(hashtable[1]); // stunggal
-		System.Console.WriteLine(hashtable[2]); // empty
-		System.Console.WriteLine(hashtable[3]); // true
-		System.Console.WriteLine(hashtable[false]); // empty
+		// System.Console.WriteLine(hashtable[1]); // stunggal
+		// System.Console.WriteLine(hashtable[2]); // empty
+		// System.Console.WriteLine(hashtable[3]); // true
+		// System.Console.WriteLine(hashtable[false]); // empty
 		
-		// ----- DICTIONARY -----
+		// // ----- DICTIONARY -----
 		
 	}
 }
