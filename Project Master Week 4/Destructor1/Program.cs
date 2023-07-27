12c57919db25852f41cefb64fabe688ed9fcb75c
bimanjayaaji <anjay4705@gmail.com>
2023-07-27 12:05:20 +0700

    week 4

---
+++ Project Master Week 4/Destructor1/Program.cs
@@ -0,0 +1,62 @@
+﻿class Program
+{
+	static void Main()
+	{
+		// // deconstructers are not being run
+		// Player player1 = new Player("player1");
+		// Player player2 = new Player("player2");
+		// Player player3 = new Player("player3"); 
+		
+		method1();
+		method2();
+		
+		static void method1()
+		{
+			Human human1 = new Human("Human 1");
+			Human human2 = new Human("Human 2"); 
+			Human human3 = new Human("human 3");
+		}
+		
+		static void method2()
+		{
+			Animal animal1 = new Animal("Animal 1");
+			Animal animal2 = new Animal("Animal 2");
+			Animal animal3 = new Animal("Animal 3");
+		}
+		
+		GC.Collect();
+		GC.WaitForPendingFinalizers();
+		
+		Console.ReadLine();
+	}
+}
+
+public class Animal
+{
+	public string myString;
+	public Animal(string something)
+	{
+		this.myString = something;
+		Console.WriteLine("Constructed " + this.myString);
+	}
+	~Animal()
+	{
+		Console.WriteLine("Deconstructed " + this.myString);
+		Human humanAnimal = new Human("Human Animal");
+	}
+}
+
+public class Human
+{
+	public string myString;
+	public Human(string something)
+	{
+		this.myString = something;
+		Console.WriteLine("Constructed " + this.myString);
+	}
+	~Human()
+	{
+		Console.WriteLine("Deconstructed " + this.myString);
+		Animal animalHuman = new Animal("Animal Human");
+	}
+}
---
+++ Project Master Week 4/DisposeP1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+
+class Program
+{
+	static void Main()
+	{
+		using (var theWriter = new TheStringWriter())
+		{
+			theWriter.WriteSmth("Hola, ");
+			theWriter.WriteSmth("LEk!");
+			theWriter.ReadSmth();
+		}
+	}
+}
+
+public class TheStringWriter : IDisposable
+{
+	private StringWriter stringWriter;
+	private bool disposed = false;
+	
+	public TheStringWriter()
+	{
+		stringWriter = new StringWriter();
+		Console.WriteLine("Constructed TheStringWriter");
+	}
+	
+	public void WriteSmth(string data)
+	{
+		stringWriter.Write(data);
+		Console.WriteLine("TheStringWriter is writing : " + data);
+	}
+	
+	public void ReadSmth()
+	{
+		Console.WriteLine("TheStringWriter is reading : " + stringWriter.ToString());
+	}
+	
+	protected virtual void Dispose(bool disposing)
+	{
+		if(!disposed)
+		{
+			stringWriter.Dispose();
+		}
+		
+		disposed = true;
+		
+		Console.WriteLine("Disposing");
+	}
+	
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
+	
+	~TheStringWriter()
+	{
+		Dispose(false);
+	}
+}
