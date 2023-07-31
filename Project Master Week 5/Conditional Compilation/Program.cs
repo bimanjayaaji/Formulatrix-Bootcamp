cbd7729d093130fee08e52e32218f50ad8c1adec
bimanjayaaji <anjay4705@gmail.com>
2023-07-31 12:05:20 +0700

    week5

---
+++ Project Master Week 5/Conditional Compilation/Program.cs
@@ -0,0 +1,24 @@
+﻿#define RELEASE
+
+class Program
+{
+	static async Task Main(string[] args)
+	{
+		#if DEBUG
+		Console.WriteLine("WINDOWS running");
+		
+		#elif RELEASE
+		Console.WriteLine("ANDROID running");
+		
+		#endif
+		string y = Console.ReadLine();
+		Console.WriteLine(y);
+		
+		while (true)
+		{
+			Console.WriteLine("Botol makan sapi");
+			Console.WriteLine("Pensilku");
+			await Task.Delay(2000);
+		}
+	}
+}
---
+++ Project Master Week 5/Logging/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine(Substract(2,3));
+		
+		Debug.WriteLine("debug writeline");
+		Trace.WriteLine("trace writeline");
+		Debug.Assert(true,"debug assert true");
+		Trace.Assert(false,"trace assert false");
+	}
+	
+	static int Substract(int x, int y)
+	{
+		return (x-y);
+	}
+}
